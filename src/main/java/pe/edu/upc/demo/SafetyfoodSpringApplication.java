a8a6f2dbdeafa2630936523a14b3d4f030e4e2a8
Alexandro1704 <u201811079@upc.edu.pe>
2022-05-30 21:52:27 -0500

    Initial commit

---
+++ src/main/java/pe/edu/upc/demo/SafetyfoodSpringApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.upc.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SafetyfoodSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SafetyfoodSpringApplication.class, args);
+	}
+
+}
